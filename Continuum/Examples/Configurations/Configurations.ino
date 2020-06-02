45bcc02523405f1189d7e87974bdae3b49b6d53c
VisakanMathy <visakan.mathivannan@emotech.co>
2020-06-03 00:16:17 +0100

    Commenting examples

--- Continuum/Examples/Configurations/Configurations.ino
+++ Continuum/Examples/Configurations/Configurations.ino
@@ -1,30 +1,44 @@
+/*************************************************** 
+  This is an example for the Continuum Robot Open source project
+  Configurations.ino - This will provide examples of different ways
+  the continuum robt can be actuated
+
+  This requires the use of I2C to communicate, 2 pins are required to  
+  interface and utilise the Adafruit PWMServo Driver
+
+  Written by Visakan Mathivannan
+ ****************************************************/
 #include <Continuum.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-Continuum robo(0, 3);
+Continuum robo(0, 3); //Create Robo instance for a 3 segment robot with starting angles at 0 degrees
 void setup()
 {
+  // MAKE SURE TO CHECK BOUNDARIES FOR YOUR SERVOS FOLLOWING THE ADVICE ON ADAFRUIT PWMSERVO LIBRARY
   Serial.begin(9600);
-  robo.pwmSetup();
-  robo.setCardinalTuner(0.5);
-  robo.setSectionTuner(0);
-
-  // put your setup code here, to run once:
+  robo.pwmSetup(); //Enable Adafruit servodriver, you may need to check frequencies, refer to Adafruit library
+  //robo.setPWMBounds(int SERVOMIN, int SERVOMAX); //Define the PWM boundary
+  robo.setCardinalTuner(0.5); // Define cardinal tuner (0-1)
+  robo.setSectionTuner(0);    // Define sectional tuner (0-1)
   delay(1000);
 }
 
 void loop()
 {
-  double x[12] = {0, 0, 0, 0, 0, 0, 0.00, 0, 00, 0, 0, 0};
-  robo.setAngles(x);
+  double x[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Explore changing values here to ensure the servo motors are correctly plugged in
+  robo.setAngles(x);                                   // Changes the actual angles
 
-  // Configuration 1
+  //Uncomment the different configurations to explore different positions and compare against library pictures
+
+  // Configuration 1 - This utilises the easy pull function define the segment you want to interact with
+  // and then the amount the servo should rotate and then the direction that you want to pull tendon towards
   //robo.easyPull(1,  180 ,80);
   //robo.easyPull(2,  45 ,0);
   //robo.easyPull(3,  120 ,200);
 
-  // Configuration 2
+  // Configuration 2 - This utilises the increment function define first the segment, then the servo angle and then
+  // the cardinal direction
   //robo.pullIncrement(3,90, 1);
   //robo.pullIncrement(1,120,3);
 
--- Continuum/Examples/WorkspaceExploration/WorkspaceExploration.ino
+++ Continuum/Examples/WorkspaceExploration/WorkspaceExploration.ino
@@ -1,3 +1,15 @@
+/*************************************************** 
+  This is an example for the Continuum Robot Open source project
+  WorkspaceExploration.ino - This conducts a full exploration of the workspace
+
+  Make sure to run through Configurations.ino first
+
+  This requires the use of I2C to communicate, 2 pins are required to  
+  interface and utilise the Adafruit PWMServo Driver
+
+  Written by Visakan Mathivannan
+ ****************************************************/
+
 #include <Continuum.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
@@ -5,40 +17,37 @@
 Continuum robo(0, 3);
 void setup()
 {
+  // SETUP ROBO - look at configurations.ino or refer to read_me
   Serial.begin(9600);
   robo.pwmSetup();
   robo.setCardinalTuner(0.4);
   robo.setSectionTuner(0);
-
-  // put your setup code here, to run once:
   delay(1000);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  robo.startPull();
-
+  //define how many different angles each segment should explore where value corresponds to segment
   double divisions1 = 2;
   double divisions2 = 2;
   double divisions3 = 2;
   for (int i1 = 0; i1 < 4; i1++)
   {
-    int range1i = 76 - robo.currentAngle(1, i1);
+    int range1i = 76 - robo.currentAngle(1, i1); // (1) Change both  Define the maximum turning point for segment 1
     for (int ang1i = 0; ang1i < divisions1; ang1i++)
     {
       robo.pullIncrement(1, range1i / divisions1, i1);
-      int range1j = 76 - robo.currentAngle(1, (i1 + 1) % 4);
+      int range1j = 76 - robo.currentAngle(1, (i1 + 1) % 4); // (1) Change both  Define the maximum turning point for segment 1
       for (int angj1 = 0; angj1 < divisions1; angj1++)
       {
         robo.pullIncrement(1, range1j / divisions1, (i1 + 1) % 4);
 
         for (int i2 = 0; i2 < 4; i2++)
         {
-          int range2i = 76 - robo.currentAngle(2, i2);
+          int range2i = 76 - robo.currentAngle(2, i2); // (2) Change both  Define the maximum turning point for segment 2
           for (int ang2i = 0; ang2i < divisions2; ang2i++)
           {
-            robo.pullIncrement(2, range2i / divisions2, i2);
+            robo.pullIncrement(2, range2i / divisions2, i2); // (2) Change both  Define the maximum turning point for segment 2
             int range2j = 76 - robo.currentAngle(2, (i2 + 1) % 4);
             for (int angj2 = 0; angj2 < divisions2; angj2++)
             {
@@ -46,11 +55,11 @@ void loop()
 
               for (int i3 = 0; i3 < 4; i3++)
               {
-                int range3i = 60 - robo.currentAngle(3, i3);
+                int range3i = 60 - robo.currentAngle(3, i3); // (3) Change both  Define the maximum turning point for segment 3
                 for (int ang3i = 0; ang3i < divisions3; ang3i++)
                 {
                   robo.pullIncrement(3, range3i / divisions3, i3);
-                  int range3j = 60 - robo.currentAngle(3, (i3 + 1) % 4);
+                  int range3j = 60 - robo.currentAngle(3, (i3 + 1) % 4); // (3) Change both  Define the maximum turning point for segment 3
                   for (int angj3 = 0; angj3 < divisions3; angj3++)
                   {
                     robo.pullIncrement(3, range3j / divisions3, (i3 + 1) % 4);
@@ -83,7 +92,7 @@ void loop()
     robo.startPull();
     delay(500);
   }
-  double x[12] = {0, 0, 0, 0, 0, 0, 0, 0, 00, 0, 0, 0};
+  double x[12] = {0, 0, 0, 0, 0, 0, 0, 0, 00, 0, 0, 0}; //ensure it returns to origin
   robo.setAngles(x);
   while (1)
   {
